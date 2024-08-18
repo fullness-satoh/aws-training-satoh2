e7e48468b9c99c12370bb78b809935995722eaba
fullness-satoh <fullness.training056@gmail.com>
2024-08-18 21:31:04 +0900

    05_urlpatternのバグ修正

--- myproject/config/urls.py
+++ myproject/config/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('upload/', FileUploadView.as_view(), name='file_upload'),
-
+]
